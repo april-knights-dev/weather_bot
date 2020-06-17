653ed1a8ca3e0d9043bb2dc1da9a7ebb425b3923
今田　優希 <58desuyo@gmail.com>
2020-06-17 21:38:10 +0900

    出力文章に変数を導入

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -3,11 +3,34 @@
 from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
 from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
+
 import os
 import requests
 import urllib.request as req
 import sys
 import json
+
+city_name = "Kobe" # 主要な都市名はいけるっぽい。
+API_KEY = "e2b220b4263af8d026cb5e44abd8f568" # xxxに自分のAPI Keyを入力。
+api = "http://api.openweathermap.org/data/2.5/weather?units=metric&q={city}&APPID={key}"
+
+url = api.format(city = city_name, key = API_KEY)
+print(url)
+response = requests.get(url)
+data = response.json()
+jsonText = json.dumps(data, indent=4)
+print(jsonText)
+
+res_api = json.loads(jsonText)
+res_main = res_api.get("main")
+res_weater = res_api.get("weater")
+res_mark = res_weater.get("main")
+res_pressure = res_main.get("pressure")
+# 辞書型の中身の取り出し方
+# dict["key_name"] or dict.get("key_name")
+
+# try  key_nameをcloudsとして上のやり方で取ってみよう
+# try main
 
 # @respond_to('string')     bot宛のメッセージ
 #                           stringは正規表現が可能 「r'string'」
@@ -25,16 +48,13 @@ import json
 # message.react('icon_emoji')  発言者のメッセージにリアクション(スタンプ)する
 #                               文字列中に':'はいらない
 
-
 # .*でどんなメッセージでも受け付ける状態
 # respond_toで指定してもいいし、中でif message=xxx と分岐してもいい
 @respond_to("天気")
 def mention_func(message):
-    message.reply("ここは東京です") # メンション
-
-
-
+ message.reply(f"今日の天気は{res_mark}です。") # メンション
 # @listen_to('リッスン')
 # def listen_func(message):
 #     message.send('誰かがリッスンと投稿したようだ')      # ただの投稿
 #     message.reply('君だね？')
+
