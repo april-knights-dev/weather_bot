2a4db0455a5ff7d69a2270c18ce2f4f86e85bd1f
8kia <8kia12238@gmail.com>
2020-08-01 17:51:57 +0900

    run.pyのTOKEN修正

--- run.py
+++ run.py
@@ -6,20 +6,20 @@ import slack
 from slackbot.bot import Bot
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-client = slack.WebClient(token=os.environ['SLACK_CLIENT_TOKEN'])
+client = slack.WebClient(token=os.environ['API_TOKEN'])
 sched = BlockingScheduler()
 
-def send_message(channel, message):
-    client.chat_postMessage(channel=channel, text=message)
+# 傘警報の起動
+def send_message(channel,message):
+    client.chat_postMessage(channel=channel,text=message)
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=1, minute=49)
-def timed_job():
-    send_message('G0149FE9SAW', "job")
+@sched.scheduled_job('cron', day_of_week='mon-sat', hour=11, minute=46)
+def job_timed():
+    send_message('G0149FE9SAW', '')
 
 def main():
     bot = Bot()
     bot.run()
-
 
 if __name__ == "__main__":
     print('start slackbot')
@@ -31,4 +31,3 @@ if __name__ == "__main__":
     # 傘警報の起動
     job = Thread(target=sched.start)
     job.start()
-
