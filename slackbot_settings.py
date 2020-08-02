a5c19e0b086f22976a3ca34f5f2872af5493058b
今田　優希 <58desuyo@gmail.com>
2020-08-02 20:45:07 +0900

    API_TOKE をBOT_TOKENに変更

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -2,7 +2,7 @@
 import os
 
 # botアカウントのトークンを指定
-API_TOKEN = os.environ["API_TOKEN"]
+API_TOKEN = os.environ["BOT_TOKEN"]
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "すみません、それはまだわかりません"
