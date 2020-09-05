87787860f1862298d6c37053d417c3578d6a887d
今田　優希 <58desuyo@gmail.com>
2020-09-05 12:17:19 +0900

    aaa

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -155,18 +155,18 @@ def reply_weather(message, arg):
         res_mark = f"設定辞書に{res_mark}が含まれてないみたいだよ"
     
     if "天気" in arg:
-        # message.reply(f"\nこんにちは！晴男です！！！\n{date_time} 現在の{city}は{res_mark}！！！\n気温は{res_temp}度です！！！") 
-        client.chat_postMessage(
-            channel=message.body['channel'],
-            blocks=message_format("*こんにちは！！晴男です！！！*",date_time + "\n現在の"+ city + "は" + res_mark + "！！！\n気温は" + res_temp + "度です！！！")
-            )
+        message.reply(f"\nこんにちは！晴男です！！！\n{date_time} 現在の{city}は{res_mark}！！！\n気温は{res_temp}度です！！！") 
+        # client.chat_postMessage(
+        #     channel=message.body['channel'],
+        #     blocks=message_format("*こんにちは！！晴男です！！！*",date_time + "\n現在の"+ city + "は" + res_mark + "！！！\n気温は" + res_temp + "度です！！！")
+        #     )
 
     if "傘" in arg :
-        # message.send(f"\nお疲れ様です！！！晴男です！！！\n\n{Today_rain}\n\n朝昼晩に分けての降水確率は、\n{Morning_rain}%\n{Noon_rain}%\n{Night_rain}%\n\n今日も一日頑張りましょう！！！")
-        client.chat_postMessage(
-            channel=message.body['channel'],
-            blocks=message_format("*お疲れ様です！！！晴男です！！！*",Today_rain +"\n\n朝昼晩に分けての降水確率は、"+ Morning_rain + "%\n" + Noon_rain + "%\n" + Night_rain +"%\n\nですよ〜！！！")
-        )
+        message.send(f"\nお疲れ様です！！！晴男です！！！\n\n{Today_rain}\n\n朝昼晩に分けての降水確率は、\n{Morning_rain}%\n{Noon_rain}%\n{Night_rain}%\n\n今日も一日頑張りましょう！！！")
+        # client.chat_postMessage(
+        #     channel=message.body['channel'],
+        #     blocks=message_format("*お疲れ様です！！！晴男です！！！*",Today_rain +"\n\n朝昼晩に分けての降水確率は、"+ Morning_rain + "%\n" + Noon_rain + "%\n" + Night_rain +"%\n\nですよ〜！！！")
+        # )
 
 # if msg in "傘" or "天気":
 #      prefectre_set.get()
@@ -188,24 +188,24 @@ def get_api_response(city):
 
   return data
 
-def message_format(aisatu,message):
-    blockkit = [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": aisatu
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": message
-                }
-            }
-        ]
-    return blockkit
+# def message_format(aisatu,message):
+#     blockkit = [
+#             {
+#                 "type": "section",
+#                 "text": {
+#                     "type": "mrkdwn",
+#                     "text": aisatu
+#                 }
+#             },
+#             {
+#                 "type": "section",
+#                 "text": {
+#                     "type": "mrkdwn",
+#                     "text": message
+#                 }
+#             }
+#         ]
+#     return blockkit
 # 辞書型の中身の取り出し方
 # dict["key_name"] or dict.get("key_name")
 
